946156903e1d578beee7dbf027d66c45cf4e7bef
Zane Prall <99636457+zaneprall@users.noreply.github.com>
2023-08-03 16:37:16 -0600

    Update ExfilQR.py
    fixed the version that contained multiprocessing. thanks AI!

--- ExfilQR.py
+++ ExfilQR.py
@@ -1,4 +1,5 @@
 import os
+import time
 import argparse
 import imageio
 import cv2
@@ -6,6 +7,7 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 import qrcode
 import numpy as np
+from multiprocessing import Pool
 
 def data_to_qr(data, size=(512, 512)):
     qr = qrcode.QRCode(
@@ -20,13 +22,15 @@ def data_to_qr(data, size=(512, 512)):
     img = np.array(img).astype(np.uint8) * 255
     return img
 
-
-def file_to_images(file_path, chunk_size=1024, verbose=False):
+def file_to_images(file_path, chunk_size=1024, verbose=False, num_processes=4):
     with open(file_path, "rb") as f:
         data = f.read()
 
     chunks = [data[i : i + chunk_size] for i in range(0, len(data), chunk_size)]
-    images = [data_to_qr(chunk) for chunk in chunks]
+
+    # Create a process pool
+    with Pool(num_processes) as p:
+        images = p.map(data_to_qr, chunks)
 
     if verbose:
         print(f"Generated {len(images)} images from file {file_path}")
@@ -63,6 +67,8 @@ def images_to_mp4(images, save_path, fps=30, verbose=False):
             writer.append_data(image)
 
 def main():
+    start_time = time.time()  # record the start time
+
     parser = argparse.ArgumentParser(description="Converts files to QR code videos and back.")
     parser.add_argument("input", help="Input file or video.")
     parser.add_argument("output", help="Output video or file.")
@@ -70,15 +76,34 @@ def main():
     parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity.")
     parser.add_argument("--chunk-size", type=int, default=1024, help="Size of the data chunks (only applicable when creating a video).")
     parser.add_argument("-b", "--backward", action="store_true", help="Enable backward operation (video to file).")
+    parser.add_argument("-p", "--num-processes", type=int, default=4, help="Number of processes to use.")
     args = parser.parse_args()
+
+    input_size = os.path.getsize(args.input)  # get the size of the input file
 
     if args.backward:
         data = video_to_data(args.input, args.verbose)
         with open(args.output, "wb") as f:
             f.write(data)
+        output_size = len(data)
     else:
-        images = file_to_images(args.input, args.chunk_size, args.verbose)
+        images = file_to_images(args.input, args.chunk_size, args.verbose, args.num_processes)
         images_to_mp4(images, args.output, args.fps, args.verbose)
+        output_size = os.path.getsize(args.output)  # get the size of the output file
+
+    end_time = time.time()  # record the end time
+
+    elapsed_time = end_time - start_time  # calculate elapsed time
+
+    print(f"Input size: {input_size / (1024 * 1024):.2f} MB")
+    print(f"Output size: {output_size / (1024 * 1024):.2f} MB")
+    print(f"Time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Speed: {input_size / elapsed_time / (1024 * 1024):.2f} MB/s")
+
+    if not args.backward:
+        duration = len(images) / args.fps
+        print(f"Video duration: {duration:.2f} seconds")
+        print(f"Bitrate: {output_size * 8 / duration / 1000:.2f} kbps")
 
 if __name__ == "__main__":
     main()
